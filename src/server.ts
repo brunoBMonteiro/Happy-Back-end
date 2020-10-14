1ba73eb84baa59f1f8d1cdf1647580061bd63824
Bruno Bilheri <52968377+brunoBMonteiro@users.noreply.github.com>
2020-10-14 18:41:03 -0300

    Abstracting in Controller

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,32 @@
 import express from 'express';
+import './database/connection';
+
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    return response.json({ message: 'Hello World' });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
+
+
+
+
+
+
+// Rota = conjunto
+// Recurso = usuario
+
+// Métodos HTTP = GET, POST, PUT, DELETE
+
+// Parâmetros
+
+// GET = Buscar uma informação (Lista, item)
+// POST = Criando uma informação
+// PUT = Editando uma informação
+// DELETE = Deletando uma informação
+
+// Query: https://localhost:3333/users?search=bruno
+// Route Params: http://localhost:3333/users/1 (Identificar um recurso)
+// Body: http://localhost:3333/users (Identificar um recurso)
