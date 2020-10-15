48e9ac66c30e33254c178742b4e4a189a8d3ee6b
Bruno Bilheri <52968377+brunoBMonteiro@users.noreply.github.com>
2020-10-14 23:09:58 -0300

    Cors installed

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
+
 import 'express-async-errors';
 
 import './database/connection';
@@ -10,6 +12,7 @@ import errorHandler from './errors/handler';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
