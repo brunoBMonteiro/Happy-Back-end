3f631936559dc57cac64fca7404f738633b8fe63
Bruno Bilheri <52968377+brunoBMonteiro@users.noreply.github.com>
2020-10-14 21:00:03 -0300

    image upload to completed DB

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -36,6 +36,11 @@ export default {
 
         const orphanagesRepository = getRepository(Orphanage);
 
+        const requestImages = request.files as Express.Multer.File[];
+        const images = requestImages.map(image => {
+            return { path: image.filename }
+        })
+
         const orphanage = orphanagesRepository.create({
             name,
             latitude,
@@ -44,6 +49,7 @@ export default {
             instructions,
             opening_hours,
             open_on_weekends,
+            images
         });
 
         await orphanagesRepository.save(orphanage);
--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from './config/upload';
 import OrphanagesController from './controllers/OrphanagesController';
 
 const routes = Router();
-
+const upload = multer(uploadConfig);
 // padrão do controler é ter os metodos 
 // index, show, create, update, delete
 
 routes.get('/orphanages', OrphanagesController.index);
 routes.get('/orphanages/:id', OrphanagesController.show);
-routes.post('/orphanages', OrphanagesController.create);
+routes.post('/orphanages', upload.array('images'), OrphanagesController.create);
 
 export default routes;
